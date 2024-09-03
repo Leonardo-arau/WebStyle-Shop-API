ffc523203b76647777f3f1da59fd8a670e1871b4
Leonardo <leo-a-oliveira@outlook.com>
2024-09-03 14:10:18 -0300

    Criando uma Loja Virtual Usando Microsserviços

---
+++ WebStyle.ProductApi/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebStyle.ProductApi.Models;
+
+namespace WebStyle.ProductApi.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { } 
+       
+        public DbSet<Category> Categories { get; set; } 
+        public DbSet<Product> Products { get; set; }
+
+        //Fluent API
+
+    }
+}
